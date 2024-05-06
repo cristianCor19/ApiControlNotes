aa0ab4ea60c657baa9d51894490b9a17b70b27dc
cristianCor19 <cordobac96@gmail.com>
2024-05-06 16:26:18 -0500

    add config firebase

---
+++ config.js
@@ -0,0 +1,10 @@
+import {config} from 'dotenv'
+config()
+
+export const FIREBASE_APIKEY  = process.env.FIREBASE_APIKEY 
+export const FIREBASE_AUTHDOMAIN  = process.env.FIREBASE_AUTHDOMAIN  
+export const FIREBASE_PROJECTID  = process.env.FIREBASE_PROJECTID  
+export const FIREBASE_STORAGEBUCKET  = process.env.FIREBASE_STORAGEBUCKET  
+export const FIREBASE_MESSAGEINGSENDERID  = process.env.FIREBASE_MESSAGEINGSENDERID  
+export const FIREBASE_APPID  = process.env.FIREBASE_APPID  
+export const FIREBASE_MEASUREMENTID  = process.env.FIREBASE_MEASUREMENTID  
