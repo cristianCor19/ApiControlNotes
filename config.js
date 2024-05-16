8a17eec835fb8d563530672d5e72b2e77b6f953e
cristianCor19 <cordobac96@gmail.com>
2024-05-16 14:58:29 -0500

    Add credentials to firebase

--- config.js
+++ config.js
@@ -8,3 +8,10 @@ export const FIREBASE_STORAGEBUCKET  = process.env.FIREBASE_STORAGEBUCKET
 export const FIREBASE_MESSAGEINGSENDERID  = process.env.FIREBASE_MESSAGEINGSENDERID  
 export const FIREBASE_APPID  = process.env.FIREBASE_APPID  
 export const FIREBASE_MEASUREMENTID  = process.env.FIREBASE_MEASUREMENTID  
+
+
+export const PROJECT_ID = process.env.PROJECT_ID
+export const PRIVATE_KEY_ID = process.env.PRIVATE_KEY_ID
+export const CLIENT_EMAIL = process.env.CLIENT_EMAIL
+export const CLIENT_ID = process.env.CLIENT_ID
+export const PRIVATE_KEY = process.env.PRIVATE_KEY
