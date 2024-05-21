359e2e942056e6d5143e1775ef844ecf90b7544b
cristianCor19 <cordobac96@gmail.com>
2024-05-21 18:55:45 -0500

    update model user

--- models/user.model.js
+++ models/user.model.js
@@ -32,7 +32,7 @@ const userSchema = new Schema(
             required: true,
             unique: true
         },
-        Subjects:[
+        subjects:[
             {
                 type: Schema.Types.ObjectId,
                 ref : 'Subject'
