c13aaf2fa21f384d1d52d578a9fde26ef6f4742f
cristianCor19 <cordobac96@gmail.com>
2024-05-21 18:55:10 -0500

    add routes of the subject

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import cors from 'cors';
 import dotenv from 'dotenv'
 import userRoutes from './v1/routes/user.routes.js'
 import sessionRoutes from './v1/routes/session.routes.js'
+import subjectRoutes from './v1/routes/subject.routes.js'
 import connectToDatabase from './drivers/connect_db.js'
 import swaggerDocs from './v1/swagger.js';
-
 
 
 
@@ -25,7 +25,8 @@ app.use(express.json())
 connectToDatabase()
 
 app.use('/user', userRoutes)
-app.use('/session', sessionRoutes)
+app.use('/session', sessionRoutes)           
+app.use('/subject', subjectRoutes)           
 
 app.listen(app.get('port'), () =>{
     console.log(`server listening to port : ${app.get('port')}`);
