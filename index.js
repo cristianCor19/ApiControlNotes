370c2edfaee027be6fb50eafd7b9f3b9634302b6
cristianCor19 <cordobac96@gmail.com>
2024-05-22 00:33:34 -0500

    add routes to the app

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import userRoutes from './v1/routes/user.routes.js'
 import sessionRoutes from './v1/routes/session.routes.js'
 import subjectRoutes from './v1/routes/subject.routes.js'
+import activityRoutes from './v1/routes/activity.routes.js'
 import connectToDatabase from './drivers/connect_db.js'
 import swaggerDocs from './v1/swagger.js';
 
@@ -27,6 +28,7 @@ connectToDatabase()
 app.use('/user', userRoutes)
 app.use('/session', sessionRoutes)           
 app.use('/subject', subjectRoutes)           
+app.use('/activity', activityRoutes)           
 
 app.listen(app.get('port'), () =>{
     console.log(`server listening to port : ${app.get('port')}`);
