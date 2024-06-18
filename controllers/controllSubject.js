e8a2a6c1c5d7a3a49ddc6b5db56d4bc8b77e011c
cristianCor19 <cordobac96@gmail.com>
2024-06-18 09:47:50 -0500

    update method create subject

--- controllers/controllSubject.js
+++ controllers/controllSubject.js
@@ -5,13 +5,28 @@ import User from '../models/user.model.js'
 export async function saveSubject(req, res) {
     try {
         const idUser = req.params.id
-        const { name, code } = req.body
-       
+        const { name, color } = req.body
+
+
+        if (!name || !color) {
+            return res.status(400).json({
+                "status": false,
+                "message": "Missing required parameters: 'name' and 'color'"
+            })
+        }
+
         const userFound = await User.findById(idUser)
+
+        if (!userFound) {
+            return res.status(404).json({
+                "status": false,
+                "message": "User not found"
+            })
+        }
 
         const newSubject = new Subject({
             name,
-            code,
+            color,
             user: idUser,
         })
 
@@ -21,7 +36,7 @@ export async function saveSubject(req, res) {
 
         const subjectSaveData = await newSubject.save()
 
-        return res.status(200).json({
+        return res.status(201).json({
             "status": true,
             "message": 'Subject saved successfully'
         })
