5083f6262afcbc3a8ce41ca75b0756caaff5fa08
cristianCor19 <cordobac96@gmail.com>
2024-06-19 17:07:52 -0500

    Update method for create activity

--- controllers/controllActivity.js
+++ controllers/controllActivity.js
@@ -8,13 +8,31 @@ import Activity from '../models/activity.model.js'
 export async function saveActivity(req, res) {
     try {
         const idSubject = req.params.id
+        console.log(idSubject);
         const { name, dateEntry, percent } = req.body
+       
+
+
+        if(!name || !dateEntry || !percent){
+            return res.status(404).json({
+                "status": "false",
+                "message": "Missing required parameters: name, dateEntry, percent and percent"
+            })
+        }
     
+
         const subjectFound = await Subject.findById(idSubject)
+        console.log(subjectFound);
+        if(!subjectFound) {
+            return res.status(400).json({
+                "status": "false",
+                "message": "Not Found subject"
+            })
+        }
 
         const parsedDateEntry = new Date(dateEntry)
 
-        //get the date creation of the activity
+    
         const currentDate =  moment.tz('America/Bogota').format()
         const newActivity = new Activity({
             name,
@@ -23,21 +41,24 @@ export async function saveActivity(req, res) {
             dateCreation: currentDate
         })
 
+
         //save activity in subject
         newActivity.subjectFound = idSubject
         subjectFound.activities.push(newActivity)
         await subjectFound.save()
+        
 
-        const subjectSaveData = await newActivity.save()
+        const activitySaveData = await newActivity.save()
 
-        return res.status(200).json({
+        return res.status(201).json({
             "status": true,
             "message": 'Activity saved successfully'
         })
     } catch (error) {
         return res.status(500).json({
             "status": false,
-            "error": error.message
+            "error": error.message,
+            "message": "Error saving activity"
         })
     }
 }
